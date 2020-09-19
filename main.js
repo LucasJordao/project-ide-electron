01caf0acc6d826e6d4aeaf9b6486a33e1a92e37b
LucasJordao <lucaswill12@hotmail.com>
2020-09-19 13:12:13 -0300

    Adicionando funções aos botões de controle da janela e alterando a abertura do programa para iniciar em tela fullsize

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
+            enableRemoteModule: true
         },
-        frame: false
+        frame: false,
+        show: false
     });
-
+    mainWindow.maximize();
+    mainWindow.show();
     mainWindow.loadURL(resolve('file://', __dirname, 'src', 'view', 'index.html'));
     
+});
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
 });
 
